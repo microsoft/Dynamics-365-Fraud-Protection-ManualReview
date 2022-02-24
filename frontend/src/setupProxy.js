98dda4b097bf90bc475ec90c2c7d1ed6161d8bb0
Manoj Nithyananda Shenoy <mashenoy@microsoft.com>
2022-02-24 14:14:09 -0800

    Gradle file changes

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,26 +1,56 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 /**
  * Setup of proxy for Webpack Dev Server to workaround CORS issues in development mode
  * @param app
  */
 module.exports = function proxyConfiguration(app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            // NOTE: in order to access local BackEnd installation
-            // specify API_BASE_URL environment variable equal 'http://localhost:8080/api',
-            // for example in .env file
-            target: process.env.API_BASE_URL || 'some url',
-            secure: false,
-            changeOrigin: true,
-            // logLevel: 'debug',
-            pathRewrite: {
-                '^/api': ''
-            }
-        })
-    );
+  app.use(
+    "/api/dashboards",
+    createProxyMiddleware({
+      // NOTE: in order to access local BackEnd installation
+      // specify API_BASE_URL environment variable equal 'http://localhost:8080/api',
+      // for example in .env file
+      target: "http://localhost:8081/api" || "https://dev-dfp-mr.azurefd.net",
+      secure: false,
+      changeOrigin: true,
+      logLevel: "debug",
+      pathRewrite: {
+        "^/api/dashboards": "dashboards",
+      },
+    })
+  );
+  app.use(
+    "/api/collected-info",
+    createProxyMiddleware({
+      // NOTE: in order to access local BackEnd installation
+      // specify API_BASE_URL environment variable equal 'http://localhost:8080/api',
+      // for example in .env file
+      target: "http://localhost:8081/api" || "https://dev-dfp-mr.azurefd.net",
+      secure: false,
+      changeOrigin: true,
+      logLevel: "debug",
+      pathRewrite: {
+        "^/api/collected-info": "collected-info",
+      },
+    })
+  );
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      // NOTE: in order to access local BackEnd installation
+      // specify API_BASE_URL environment variable equal 'http://localhost:8080/api',
+      // for example in .env file
+      target: "http://localhost:8080/api" || "https://dev-dfp-mr.azurefd.net",
+      secure: false,
+      changeOrigin: true,
+      logLevel: "debug",
+      pathRewrite: {
+        "^/api": "",
+      },
+    })
+  );
 };
